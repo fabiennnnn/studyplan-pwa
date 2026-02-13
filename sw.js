45deea214855b8aa67302a4e23ea238c09d96cbd
ZHANGFAN <fabien@ZHANGFANdeMacBook-Air.local>
2026-02-13 23:22:21 +0900

    Initial PWA app

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'studyplan-v2';
+const CORE_ASSETS = [
+  './',
+  './studyplan.html',
+  './manifest.json',
+  './app-icon.svg',
+  './offline.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const { request } = event;
+  const url = new URL(request.url);
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./studyplan.html', copy));
+          return res;
+        })
+        .catch(() => caches.match('./offline.html'))
+    );
+    return;
+  }
+
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(request).then(cached =>
+        cached || fetch(request).then(res => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+          return res;
+        })
+      )
+    );
+  }
+});
